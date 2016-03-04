87bf2e7503dda1d30dff13b16751ed640457305f
vova <wishmaster2310@gmail.com>
2016-03-04 18:54:07 +0500

    add new content

--- public/javascripts/general.js
+++ public/javascripts/general.js
@@ -15,4 +15,20 @@ $(function() {
 	$('.j-lazybg').each(function(indx, elem) {
 			loadSectionBgs(elem);
 	});
+
+	$('.j-listtoggle').on('click', function(e) {
+		e.preventDefault();
+		var parent = $(this).closest('.wrapped-section');
+		var popover = $(parent).find('.wrapped-section__popover');
+		console.log(popover)
+
+		$(popover).toggleClass('wrapped-section__popover--active');
+	});
+
+	$('.wrapped-section__times').on('click', function(e) {
+		e.preventDefault();
+		var popover = $(this).closest('.wrapped-section__popover');
+
+		$(popover).removeClass('wrapped-section__popover--active');
+	});
 });
